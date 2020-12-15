4506ab31a162d71ec3a7fcaadd9a926dc71c6616
Derek Arrotta <derek.arrotta@gmail.com>
2020-12-15 16:17:22 -0500

    add html

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"; 
+import React, { Component } from "react";
 
 //add note
 // implements a form to capture the name, content, and folder for a new note
@@ -6,14 +6,43 @@ import React, { Component } from "react";
 // add validation to ensure name of note is not left blank
 // select folder from list of existing ones
 // ensure errors are properly handled
-// add button to note list page to invoke new form. 
+// add button to note list page to invoke new form.
 
 export default class AddNote extends Component {
+  
+  
+  
   render() {
+    const { folders = [] } = this.context;
     return (
-      <form>
-        <div>Note</div>
-      </form>
-    )
+      <section className="AddNote">
+        <h2>Create a note</h2>
+        <NotefulForm onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label htmlFor="note-name-input">Name</label>
+            <input type="text" id="note-name-input" name="note-name" />
+          </div>
+          <div className="field">
+            <label htmlFor="note-content-input">Content</label>
+            <textarea id="note-content-input" name="note-content" />
+          </div>
+          <div className="field">
+            <label htmlFor="note-folder-select">Folder</label>
+            <select id="note-folder-select" name="note-folder-id">
+              <option value={null}>...</option>
+              {folders.map((folder) => (
+                <option key={folder.id} value={folder.id}>
+                  {folder.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="buttons">
+            <button type="submit">Add note</button>
+          </div>
+        </NotefulForm>
+      </section>
+    );
   }
+
 }
