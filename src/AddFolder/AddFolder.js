2bd307da8ea192ae3f7c884bd2719610338c8606
Derek Arrotta <derek.arrotta@gmail.com>
2020-12-15 16:09:31 -0500

    add html

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,15 +1,29 @@
-import React, { Component } from "react"; 
+import React, { Component } from "react";
+import NotefulForm from '../NotefulForm/NotefulForm'
 
 //add folder
 // capture name of new folder to POST /folders endpoint server (ensure any errors are handled)
 // add a button to the navigation to invoke the new form
-// 
+//
 export default class AddFolder extends Component {
+  
+  
+
   render() {
     return (
-      <form>
-        <div>folder</div>
-      </form>
-    )
+      <section className="AddFolder">
+        <h2>Create a folder</h2>
+        <NotefulForm onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label htmlFor="folder-name-input">Name</label>
+            <input type="text" id="folder-name-input" name="folder-name" />
+          </div>
+          <div className="buttons">
+            <button type="submit">Add folder</button>
+          </div>
+        </NotefulForm>
+      </section>
+    );
   }
+
 }
