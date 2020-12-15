23f7f4a5bdd7f6d7f241cd3b296a0304f7df688d
Derek Arrotta <derek.arrotta@gmail.com>
2020-12-15 16:18:05 -0500

    add handle submit

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -16,6 +16,34 @@ export default class AddNote extends Component {
   };
   static contextType = ApiContext;
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const newNote = {
+      name: e.target["note-name"].value,
+      content: e.target["note-content"].value,
+      folderId: e.target["note-folder-id"].value,
+      modified: new Date(),
+    };
+    fetch(`${config.API_ENDPOINT}/notes`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newNote),
+    })
+      .then((res) => {
+        if (!res.ok) return res.json().then((e) => Promise.reject(e));
+        return res.json();
+      })
+      .then((note) => {
+        this.context.addNote(note);
+        this.props.history.push(`/folder/${note.folderId}`);
+      })
+      .catch((error) => {
+        console.error({ error });
+      });
+  };
+
   render() {
     const { folders = [] } = this.context;
     return (
