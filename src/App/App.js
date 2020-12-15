e53a7f3a93b59214407e6eac9da21c6703a89cc0
Derek Arrotta <derek.arrotta@gmail.com>
2020-12-15 16:22:58 -0500

    add-folder and add-note put in renderMainRoutes

--- src/App/App.js
+++ src/App/App.js
@@ -79,6 +79,8 @@ class App extends Component {
           <Route exact key={path} path={path} component={NoteListMain} />
         ))}
         <Route path="/note/:noteId" component={NotePageMain} />
+        <Route path="/add-folder" component={AddFolder} />
+        <Route path="/add-note" component={AddNote} />
       </>
     );
   }
