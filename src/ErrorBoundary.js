5d75ce499d1b508d53af6220d6efbbcbb1fec0a1
Derek Arrotta <derek.arrotta@gmail.com>
2020-12-17 15:43:03 -0500

    add propTypes validationc

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
 export default class ErrorBoundary extends Component {
 
@@ -23,3 +24,7 @@ export default class ErrorBoundary extends Component {
   } 
 
 }
+
+ErrorBoundary.propTypes = {
+  hasError: PropTypes.bool,
+};
