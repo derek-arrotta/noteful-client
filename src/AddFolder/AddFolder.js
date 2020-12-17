9c35be1a2c520e6d19d2e7dda53c8b2bef9c325e
Derek Arrotta <derek.arrotta@gmail.com>
2020-12-17 11:56:10 -0500

    delete comments

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -4,10 +4,7 @@ import ApiContext from '../ApiContext'
 import config from '../config'
 import './AddFolder.css'
 
-//add folder
-// capture name of new folder to POST /folders endpoint server (ensure any errors are handled)
-// add a button to the navigation to invoke the new form
-//
+
 export default class AddFolder extends Component {
   static defaultProps = {
     history: {
